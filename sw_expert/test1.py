bc2e87ef654f5723fb2fb7d89815669bdb82e79f
woongveloper <woongveloper98@gmail.com>
2024-01-16 17:52:18 +0900

    update240116_1

--- sw_expert/test1.py
+++ sw_expert/test1.py
@@ -1,15 +1,9 @@
-T = int(input())
-for test_case in range(1, T + 1):
-    item_number = int(input())
-    price_list = list(map(int, input().split()))
-    buy_list = []
-    for i in range(0, item_number):
-        if price_list[i] > price_list[i+1]:
-            pass
-        elif price_list[i] == price_list[i+1]:
-            buy_list.apend()
-        else:
-            plus_list = price_list[i+1] * len(buy_list)
-            plus = sum(plus_list - buy_list)         
-print(plus)
-            
+alpha = input()
+list_alpha = []
+ord_alpha = []
+for i in range(0, len(alpha)):
+	list_alpha.append((alpha[i]))
+    ord_alpha.append(list_alpha[i])
+    
+ord_alpha = ord_alpha - [ord('A')-1]*len(list_alpha)
+print(ord_alpha)
