2f4a4135f7965b1834722415deafef45a11a9ca0
woongveloper <woongveloper98@gmail.com>
2024-01-22 23:36:32 +0900

    update

--- python/test.py
+++ python/test.py
@@ -1,4 +1,17 @@
-def detail_num(x):
-    return list(range(1,x+1))
-    
-print(detail_num(3))
+t = int(input())
+for i in range(1, t+1):
+    a = list(map(str, input()))
+    for j in range(1,10):
+        if a[0:j] == a[j:2*j] and len(a[0:j]):
+            if len(a[0:j]) == 2:
+            	print(f"#{i} {2}")
+            
+            elif len(a[0:j]) == 3:
+                print(f"#{i} {3}")
+
+            elif len(a[0:j]) == 4:
+                print(f"#{i} {4}")
+            print(f"#{i} {len(a[0:j])}")
+            continue
+        elif a[0:j] == a[j:2*j] and len(a[0:j]) >= 5:
+            print(f"#{i} {len(a[0:j])}")
