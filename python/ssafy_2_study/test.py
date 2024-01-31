8988372eca34227a3958eda4d268ad9c03f590a6
woongveloper <woongveloper98@gmail.com>
2024-01-31 23:30:35 +0900

    UPDATE_AFTERSTUDY0131

--- python/ssafy_2_study/test.py
+++ python/ssafy_2_study/test.py
@@ -1,23 +1,22 @@
-def quick_sort(list):
-    if len(list) <= 1:
-        return list
+T = int(input())
+name_score = []
+count_list = [0] * 101 #0을 포함
+
+for tc in range(1,T+1):
+    name_score += [list(map(str, input().split()))] # 입력 값 받기
     
-    pivot = list[0]
-    tail = list[1:]
+score_list = []
+for i in range(0, T):
+    score_list = score_list + [int(name_score[i][1])] # 성적을 받고 이를 리스트화 (str으로 받았으니 int로 다시 전환하는 과정 필)
 
-    left_side = [num for num in tail if num <= pivot]
-    right_side = [num for num in tail if num > pivot]
+for j in range(0,100): # 해당 점수가 몇번 나왔는지 표시하는 과정
+    for num in score_list:
+        if num == j:
+            count_list[j] += 1
 
-    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
-
-N, K = map(int, input().split()) # N, K, A, B를 받는 과정
-list_A = quick_sort(list(map(int, input().split())))
-list_B = quick_sort(list(map(int, input().split())))
-
-for k in range(K): # K번 스와핑 가능하므로 K번 수행
-    list_A[k], list_B[(N-1)-k] = list_B[(N-1)-k], list_A[k] # quick_sort 한 A의 첫째 항(min)과 B의 마지막 항(max)를 스와핑
-
-result = 0
-for num in list_A:
-    result = result + num
-print(result)
+for index in range(0,len(count_list)): # count_list를 탐색하는 과정
+    if count_list[index] != 0: 
+        count_list[index] -= 1 
+        for k in range(0, len(name_score)): # 0이 아닌경우(언급이 하나라도 된 경우), 1 빼고 돌리는 for 문
+            if name_score[k][1] == str(index): # 위에서 입력받은 이름과 성적 리스트의 성적이 index와 같다면 (str으로 받음)
+                print(name_score[k][0], end = ' ') # 그 값과 일치하는 이름을 출력한다.
