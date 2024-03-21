7272bfd28dbf0a22bee5a2d22799dddc4d09b01a
woongveloper <woongveloper98@gmail.com>
2024-03-22 00:36:51 +0900

    robot_finish

--- python/HSAT대비/로봇이지나간경로.py
+++ python/HSAT대비/로봇이지나간경로.py
@@ -1,10 +1,9 @@
-from pprint import pprint
-
 # 해당 조건을 만족하려면 한 바퀴를 돌거나, 꼭지점부터 시작해서 끝 꼭지점으로 도착하면 된다.
 # 즉, 꼬다리가 있으면 꼬다리에서 시작하면 무조건 결론을 도출한다.
     # 꼬다리는 해당 블럭을 기준으로 3방향에 아무것도 없으면 이를 시작점으로 볼 수 있다.
-# 하지만, 꼬다리가 없다면? 이는 사이클을 도는 경우라고 볼 수 있다.
-# 즉, 이 경우에는 꼭짓점, matrix를 받을 때 처음으로 마주하는 '#'을 시작점으로 보면 된다
+# 또, 로봇은 두칸씩만 이동 가능하므로 사이클을 구성하는 경우는 존재하지 않는다. 
+# 따라서 꼬다리를 찾고 이를 기점으로 시작하는 것으로 문제풀이 진행
+# 아래는 꼬다리를 찾는 과정 없이 진행하면 오류가 생기는 경우
 '''
 7 5 
 ..###
@@ -17,28 +16,10 @@ from pprint import pprint
 이 경우에 (0,2)가 아닌 (2,0)이 나오도록 시작지점을 잡는 과정 필요
 '''
 
-def find_start(arr):
-    tmp = []
+def find_start(arr): # 시작지점을 찾는 함수
     for i in range(H):
         for j in range(W):
-            if arr[i][j] == '#':
-                if tmp == []:
-                    for dir in range(4):
-                        mi = i + delta[dir][0]
-                        mj = j + delta[dir][1]
-                        if 0<=mi<H and 0<=mj<W:
-                            if matrix[mi][mj] == '#':
-                                if dir == 0: # 상
-                                    tmp.append((i,j,'^'))
-                                elif dir == 1: # 하
-                                    tmp.append((i,j,'v'))
-                                elif dir == 2: # 좌
-                                    tmp.append((i,j,'<'))
-                                else:
-                                    tmp.append((i,j,'>'))
-                        if tmp != []:
-                            break
-                                # 탐색을 하면서 만약 꼬다리를 찾지 못한다면 이 값을 리턴할 예정
+            if arr[i][j] == '#':           
                 cnt = 0
                 dir_tmp = []
                 for dir in range(4):
@@ -48,7 +29,6 @@ def find_start(arr):
                         if matrix[mi][mj] == '#':
                             cnt += 1
                             if dir_tmp == []:
-                                # print(mi,mj,dir)
                                 if dir == 0: # 상
                                     dir_tmp.append((i,j,'^'))
                                 elif dir == 1: # 하
@@ -57,11 +37,10 @@ def find_start(arr):
                                     dir_tmp.append((i,j,'<'))
                                 else:
                                     dir_tmp.append((i,j,'>'))
-                if cnt >= 2:
-                    break
+                    if cnt >= 2: # 도중에 두개 이상이 cnt? 이는 꼬다리가 아님
+                        break
                 if cnt == 1: # 꼬다리를 찾았으니 이 값을 리턴
                     return dir_tmp[0]
-    return tmp[0]
             
 delta = [(-1,0),(1,0),(0,-1),(0,1)] # 상하좌우
 '''
@@ -70,7 +49,7 @@ delta = [(-1,0),(1,0),(0,-1),(0,1)] # 상하좌우
 좌로 이동 중 L:하, R:상
 우로 이동 중 L:상, R:하
 '''
-def change_dir(current_dir, target_dir):
+def change_dir(current_dir, target_dir): # 현 방향과 갈 방향이 다른 경우 명령을 찾는 함수
     if current_dir == (-1,0): # 상으로 이동 중이라면
         if target_dir == (0,-1):
             return 'L'
@@ -107,7 +86,7 @@ def move_robot(si,sj):
     # 이 경우 다시 돌아가는 경우는 고려하지 않으므로 스택은 필요없음.
     while True:
         for dir in range(4):
-            mi2 = ci + 2*delta[dir][0]
+            mi2 = ci + 2*delta[dir][0] # 2칸이동/1칸이동 모두 갈 수 있는지 고려
             mj2 = cj + 2*delta[dir][1]
             mi1 = ci + delta[dir][0]
             mj1 = cj + delta[dir][1]
@@ -117,7 +96,7 @@ def move_robot(si,sj):
                     matrix[mi2][mj2] = '.'
                     ci = mi2
                     cj = mj2
-                    if current_dir != dir:
+                    if current_dir != dir: # 방향이 다른경우 위에서 정의한 함수 활용
                         order_str += change_dir(delta[current_dir], delta[dir])
                     current_dir = dir
                     order_str += 'A'
@@ -133,13 +112,13 @@ H, W = map(int,input().split())
 
 for _ in range(H):
     matrix.append(list(input()))
-# pprint(matrix)
+
 start = find_start(matrix)
 first_dir = start[2]
-order_str = ''
+order_str = '' # move_robot 내 global 정의해서 이를 나중 명령어로 출력
 move_robot(start[0],start[1])
 
-print(start[0]+1,end = ' ')
+print(start[0]+1,end = ' ') # +1을 하는 이유 : python인식 idx != 문제의 idx
 print(start[1]+1)
 print(first_dir)
 print(order_str)
